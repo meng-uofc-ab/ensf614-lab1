5e8bd5e1457dce4ed5e32945e5f5918e871b6e73
Abhilash Paul <paul.abhilash@hotmail.com>
2024-09-15 11:21:20 -0600

    adds comments

--- exercise-d/Human.h
+++ exercise-d/Human.h
@@ -16,13 +16,32 @@ protected:
     char *name;       // Human's name
 public:
     Human(const char* nam="", double x=0, double y=0);
+    // Requires: A C-style string for name (default empty),
+    // and two doubles for x and y coordinates (default 0)
+    // Promises: Constructs a Human object with the given name and location
+    
     Human(const Human& source);
+    // Requires: A const reference to another Human object
+    // Promises: Creates a new Human object as a deep copy of the source object
+    
     Human& operator=(const Human& source);
+    // Requires: A const reference to another Human object
+    // Promises: Assigns the contents of the source object to this object, performing a deep copy
+    
     ~Human();
+    // Promises: Destroys the Human object and frees any dynamically allocated memory
+    
     char* get_name() const;
+    // Promises: Returns a pointer to the name of the Human
+    
     void set_name(const char* name);
+    // Promises: Returns the location of the Human as a Point object
+    
     Point get_point() const;
+    // Promises: Returns the location of the Human as a Point object
+    
     void display() const;
+    // Promises: Displays the Human's name and location information
 };
 
 #endif /* Human_h */
--- exercise-d/Point.h
+++ exercise-d/Point.h
@@ -11,14 +11,28 @@
 #include <stdio.h>
 using namespace std;
 
+// Class to handle a point in a cartesian plane
 class Point{
 private:
     double x, y;
 public:
     Point(double a =0, double b =0);
+    // Requires: Two double values (default to 0 if not provided)
+    // Promises: Constructs a Point object with the given x and y coordinates
+    
     double get_x() const;
+    // Promises: Returns the x-coordinate of the Point
+    
     double get_y() const;
+    // Promises: Returns the y-coordinate of the Point
+    
     void set_x(double a);
+    // Requires: A double value representing the new x-coordinate
+    // Promises: Sets the x-coordinate of the Point to the given value
+    
     void set_y(double a);
+    // Requires: A double value representing the new y-coordinate
+    // Promises: Sets the y-coordinate of the Point to the given value
+    
 };
 #endif /* Point_h */
